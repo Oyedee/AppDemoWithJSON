c1f88faa74dd6d726b259d105e60afcea3bd8ac8
Abdulhamid Oyedele Oyempemi <oyempemia@gmail.com>
2021-07-31 17:25:50 +0100

    Binding our UI components to backend

--- app/src/main/java/com/example/appdemowithjson/MainActivity.java
+++ app/src/main/java/com/example/appdemowithjson/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.appdemowithjson;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button fetchData;
+    private TextView displayData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fetchData = findViewById(R.id.button_fetch_data);
+        displayData = findViewById(R.id.text_display_data);
+
+        fetchData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
